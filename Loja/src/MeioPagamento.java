fbb008e49a986e4fde8f73f9e8f94f517e8663c9
evandra <almeidaevandra@gmail.com>
2016-12-14 17:17:10 -0200

    d

---
+++ Loja/src/MeioPagamento.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mary
+ */
+public class MeioPagamento {
+    
+    private int id;
+    private String meioPagemento;
+
+    public MeioPagamento(int id, String meioPagemento) {
+        this.id = id;
+        this.meioPagemento = meioPagemento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMeioPagemento() {
+        return meioPagemento;
+    }
+
+    public void setMeioPagemento(String meioPagemento) {
+        this.meioPagemento = meioPagemento;
+    }
+    
+    
+    
+}
